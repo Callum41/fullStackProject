a62c447b1a797aff2bc8a7eec8dc964ff5d6a6da
callum41 <ccallum0041@gmail.com>
2025-07-26 15:31:50 +0200

    progressed with the classes

---
+++ src/main/java/com/example/demo/service/ProductService.java
@@ -0,0 +1,25 @@
+package com.example.demo.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.example.demo.model.Product;
+import com.example.demo.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
+
+    public Product getProductById(Long id) {
+        return productRepository.findById(id).orElseThrow(() -> new RuntimeException("Product not found"));
+    }
+    
+}
